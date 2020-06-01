948530dd5f5e803aa504c5057ccb296da8c4bdb1
Emma Park <emma@alertlabs.com>
2020-05-31 20:11:08 -0400

    abstract functions out and add empty feed msg

--- client/client.js
+++ client/client.js
@@ -19,17 +19,24 @@ submission.addEventListener('submit', (event) => {
         content
     };
 
-    fetch(API_URL, { //posting to server, like $POST in jquery
-      method: 'POST',
-      body: JSON.stringify(coronaPost), //turning a js object into something the server can parse and understand
-      headers: {
-        'content-type': 'application/json'
-      }
-    }).then(response => response.json())  
-      .then(createdCoronaPost => {
-        submission.reset();
-        listAllcoronaPostList();
-    });
+    const input = {
+      "Title": name,
+      "Subject": content
+    };
+
+    if(isNotEmpty(input)) {
+      fetch(API_URL, { //posting to server, like $POST in jquery
+        method: 'POST',
+        body: JSON.stringify(coronaPost), //turning a js object into something the server can parse and understand
+        headers: {
+          'content-type': 'application/json'
+        }
+      }).then(response => response.json())
+        .then(createdCoronaPost => {
+          submission.reset();
+          listAllcoronaPostList();
+        });
+    }
 });
 
 search.addEventListener('submit', (event) => {
@@ -38,75 +45,85 @@ search.addEventListener('submit', (event) => {
     const searchParameter = formDataSearch.get('search');
 
     let params = `?queryString=${searchParameter}`;
-    searchURL = API_URL_SEARCH + params;
+    const searchURL = API_URL_SEARCH + params;
 
-    fetch(searchURL, {
+    const input = { "Search": searchParameter };
+    if(isNotEmpty(input)) {
+      fetch(searchURL, {
         method: 'GET',
         headers: {
-            'content-type': 'application/json'
+          'content-type': 'application/json'
         }
-    }).then(response => response.json())
-      .then(coronaPostList => {
-        coronaPostListElement.innerHTML = ''; //removes set of html dom elements WHY DOES THIS WORK
-        coronaPostList.reverse(); //reverse order - most recent tweets at the top
-        coronaPostList.forEach(coronaPost => { //for every element in this array, want to add it to page
-            const div = document.createElement('div');
-            div.className = "post-preview";
+      }).then(response => response.json())
+        .then(coronaPostList => {
+            search.reset();
+            coronaPostListElement.innerHTML = ''; //removes set of html dom elements
 
-            const header = document.createElement('h2');
-            header.className = "post-title";
-            header.textContent = coronaPost.name;
-
-            const contents = document.createElement('p');
-            contents.className = "post-subtitle";
-            contents.textContent = coronaPost.content;
-
-            const date = document.createElement('small');
-            date.className = "post-meta";
-            date.textContent = new Date(coronaPost.created);
-
-            div.appendChild(header); //each div has children of header n contents
-            div.appendChild(contents);
-            div.appendChild(date);
-            div.appendChild(document.createElement('hr'));
-
-            coronaPostListElement.appendChild(div);
-      search.reset();
-      
-  });
-});
+            if(coronaPostList.length !== 0) {
+              displayPosts(coronaPostList);
+            } else {
+              displayEmptyFeed();
+            }
+        });
+    }
 });
 
 function listAllcoronaPostList(){ //making a get req
-    coronaPostListElement.innerHTML = ''; //removes set of html dom elements WHY DOES THIS WORK
+    coronaPostListElement.innerHTML = ''; //removes set of html dom elements
     fetch(API_URL) //lel whats going on here - watch vid, calling app.get
         .then(response => response.json())
         .then(coronaPostList => {
-
-            coronaPostList.reverse(); //reverse order - most recent tweets at the top
-            coronaPostList.forEach(coronaPost => { //for every element in this array, want to add it to page
-                const div = document.createElement('div');
-                div.className = "post-preview";
-
-                const header = document.createElement('h2');
-                header.className = "post-title";
-                header.textContent = coronaPost.name;
-
-                const contents = document.createElement('p');
-                contents.className = "post-subtitle";
-                contents.textContent = coronaPost.content;
-
-                const date = document.createElement('small');
-                date.className = "post-meta";
-                date.textContent = new Date(coronaPost.created);
-
-                div.appendChild(header); //each div has children of header n contents
-                div.appendChild(contents);
-                div.appendChild(date);
-                div.appendChild(document.createElement('hr'));
-                
-                coronaPostListElement.appendChild(div);
-
-            })
+            displayPosts(coronaPostList);
         });
+}
+
+function displayPosts(postList) {
+  postList.reverse(); //reverse order - most recent tweets at the top
+  postList.forEach(coronaPost => { //for every element in this array, want to add it to page
+    const div = document.createElement('div');
+    div.className = "post-preview";
+
+    const header = document.createElement('h2');
+    header.className = "post-title";
+    header.textContent = coronaPost.name;
+
+    const contents = document.createElement('p');
+    contents.className = "post-subtitle";
+    contents.textContent = coronaPost.content;
+
+    const date = document.createElement('small');
+    date.className = "post-meta";
+    date.textContent = new Date(coronaPost.created);
+
+    div.appendChild(header); //each div has children of header n contents
+    div.appendChild(contents);
+    div.appendChild(date);
+    div.appendChild(document.createElement('hr'));
+
+    coronaPostListElement.appendChild(div);
+  })
+}
+
+function displayEmptyFeed() {
+  const div = document.createElement('div');
+  const contents = document.createElement('p');
+  contents.className = "post-subtitle";
+  contents.textContent = "Your feed is empty at the moment :'(";
+
+  div.appendChild(contents);
+  coronaPostListElement.appendChild(div);
+}
+
+/** Function checks inputs to verify that they are not empty
+ *  @param: inputs(object) and object containing the inputFieldName and value
+ *  returns a boolean
+ **/
+function isNotEmpty(inputs) {
+  for (const inputFieldName in inputs) {
+    if (inputs[inputFieldName] === "") {
+      alert(`${inputFieldName} must be filled out`);
+      return false;
+    }
+  }
+  return true;
 }
